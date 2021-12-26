bf3c58957561bdca7fef986c1f181835e65184fc
ruydiy <radoslavtcankov@abv.bg>
2021-12-26 15:08:17 +0200

    Make Models

--- carRent/app/Models/User.php
+++ carRent/app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Car;
+
 
 class User extends Authenticatable
 {
@@ -58,4 +60,10 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
+
 }
