6bb7872bb331bb33554cac6298cd10d3eaa2c43b
ruydiy <radoslavtcankov@abv.bg>
2022-01-24 16:54:37 +0200

    upload image and edit home view

--- carRent/resources/views/edit.blade.php
+++ carRent/resources/views/edit.blade.php
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="form-group">
-                            <button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update Car Info</button>
+                            <button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Update Car Info</button>
                             <a href="{{url('/show')}}" class="btn btn-default">back</a>
                         </div>
                         {{ csrf_field() }}
